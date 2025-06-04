c6ceb5c8fdb9f2ea1e302030c2ae5dbd20254464
Devon Stewart <blast@hardchee.se>
2025-06-04 14:54:03 -0700

    telescope performance is remarkably abysmal

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -68,7 +68,7 @@ vim.api.nvim_set_keymap(
 
 -- Telescope keybindings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fg', function()
   builtin.live_grep({
