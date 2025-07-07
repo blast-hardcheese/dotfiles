df447d87a63d223b0c8a275259f3ba1283eaac70
Devon Stewart <blast@hardchee.se>
2025-07-07 15:53:08 -0700

    Only attempt to configure gitsigns if it is present

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -125,78 +125,87 @@ telescope.load_extension("file_browser")
 vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>", { noremap = true })
 
 
-require('gitsigns').setup{
-  on_attach = function(bufnr)
-    local gitsigns = require('gitsigns')
+local has_plugin = function(plugin)
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/'..plugin
+  local path_empty = fn.empty(fn.glob(install_path)) == 1
+  return not path_empty
+end
 
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
+if false and has_plugin('gitsigns.vim') then
+  require('gitsigns').setup{
+    on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({']c', bang = true})
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end)
+
+      map('n', '[c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({'[c', bang = true})
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end)
+
+      map('n', ']q', function()
+          pcall(vim.cmd.cnext)
+      end)
+      map('n', '[q', function()
+          pcall(vim.cmd.cprevious)
+      end)
+
+      -- Actions
+      map('n', '<leader>hs', gitsigns.stage_hunk)
+      map('n', '<leader>hr', gitsigns.reset_hunk)
+
+      map('v', '<leader>hs', function()
+        gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end)
+
+      map('v', '<leader>hr', function()
+        gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end)
+
+      map('n', '<leader>hS', gitsigns.stage_buffer)
+      map('n', '<leader>hR', gitsigns.reset_buffer)
+      map('n', '<leader>hp', gitsigns.preview_hunk)
+      map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+      map('n', '<leader>hb', function()
+        gitsigns.blame_line({ full = true })
+      end)
+
+      map('n', '<leader>hd', gitsigns.diffthis)
+
+      map('n', '<leader>hD', function()
+        gitsigns.diffthis('~')
+      end)
+
+      map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+      map('n', '<leader>hq', gitsigns.setqflist)
+
+      -- Toggles
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+      map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+      -- Text object
+      map({'o', 'x'}, 'ih', gitsigns.select_hunk)
     end
-
-    -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
-      else
-        gitsigns.nav_hunk('next')
-      end
-    end)
-
-    map('n', '[c', function()
-      if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
-      else
-        gitsigns.nav_hunk('prev')
-      end
-    end)
-
-    map('n', ']q', function()
-        pcall(vim.cmd.cnext)
-    end)
-    map('n', '[q', function()
-        pcall(vim.cmd.cprevious)
-    end)
-
-    -- Actions
-    map('n', '<leader>hs', gitsigns.stage_hunk)
-    map('n', '<leader>hr', gitsigns.reset_hunk)
-
-    map('v', '<leader>hs', function()
-      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-    end)
-
-    map('v', '<leader>hr', function()
-      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-    end)
-
-    map('n', '<leader>hS', gitsigns.stage_buffer)
-    map('n', '<leader>hR', gitsigns.reset_buffer)
-    map('n', '<leader>hp', gitsigns.preview_hunk)
-    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
-
-    map('n', '<leader>hb', function()
-      gitsigns.blame_line({ full = true })
-    end)
-
-    map('n', '<leader>hd', gitsigns.diffthis)
-
-    map('n', '<leader>hD', function()
-      gitsigns.diffthis('~')
-    end)
-
-    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
-    map('n', '<leader>hq', gitsigns.setqflist)
-
-    -- Toggles
-    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-    map('n', '<leader>tw', gitsigns.toggle_word_diff)
-
-    -- Text object
-    map({'o', 'x'}, 'ih', gitsigns.select_hunk)
-  end
-}
+  }
+end
 
 vim.keymap.set("n", "<leader>go", ":silent exec('Git open -b cur -l ' . line('.') . ' %')<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>gd", ":silent Gdiffsplit<CR>", { noremap = true })
