6410ffacb8d01a6c6cbba85593c22bf05f9d3156
Devon Stewart <blast@hardchee.se>
2025-07-29 13:04:32 -0700

    Set tiltfile syntax to Starlark

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,3 +1,11 @@
 vim.cmd [[ colorscheme solarized ]]
 
 vim.cmd [[ set number ]]
+
+-- Tilt config files use starlark language (bazel, etc)
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = "*Tiltfile",
+  callback = function()
+    vim.bo.filetype = "starlark"
+  end,
+})
